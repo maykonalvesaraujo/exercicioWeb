234ea7f836a36cd7d14f27b6ce29384576dd3337
maykon <nokiam13@hotmail.com>
2020-03-01 16:59:09 -0300

    Configurando Sequelize, Migration usuário, Model usuario, criando loader de models, cadastro d usuários, gerando hash de senha, Autenticação jsonwebtoken, middleware de autenticação, update do usuário, validando dados de entreda.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,27 +1,22 @@
 module.exports = {
   env: {
     es6: true,
-    node: true
+    node: true,
   },
-  extends: ["airbnb-base", "prettier", "prettier/react"],
+  extends: ['airbnb-base','prettier']
   globals: {
-    Atomics: "readonly",
-    SharedArrayBuffer: "readonly"
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
   },
-  parser: "babel-eslint",
+  parser: 'babel-eslint',
   parserOptions: {
     ecmaVersion: 2018,
-    sourceType: "module"
+    sourceType: 'module',
   },
   rules: {
-    "class-methods-use-this": "off",
-    "no-param-reassgn": "off",
-    camaelcase: "off",
-    "no-unused-vars": [
-      "error",
-      {
-        argsIgnorePattern: "next"
-      }
-    ]
-  }
+    'class-methods-use-this': 'off',
+    'no-param-reassign': 'off',
+    'camelcase': 'off',
+    'no-unused-vars': ['error', { 'argsIgnorePattern': 'next' }],
+  },
 };
